d6cee36574f20998bfbc2dd1c9e9d07f0a39961f
Dutn <trandu93@outlook.com>
2018-01-19 13:18:50 +0700

    update models

---
+++ app/src/main/java/com/miuty/slowgit/data/model/Owner.java
@@ -0,0 +1,64 @@
+package com.miuty.slowgit.data.model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Owner {
+
+    @SerializedName("login")
+    private String login;
+
+    @SerializedName("id")
+    private long id;
+
+    @SerializedName("avatar_url")
+    private String avatarUrl;
+
+    @SerializedName("gravatar_id")
+    private String gravatarId;
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("html_url")
+    private String htmlUrl;
+
+    @SerializedName("followers_url")
+    private String followersUrl;
+
+    @SerializedName("following_url")
+    private String followingUrl;
+
+    @SerializedName("gists_url")
+    private String gistsUrl;
+
+    @SerializedName("starred_url")
+    private String starredUrl;
+
+    @SerializedName("subscriptions_url")
+    private String subscriptionsUrl;
+
+    @SerializedName("organizations_url")
+    private String organizationsUrl;
+
+    @SerializedName("repos_url")
+    private String reposUrl;
+
+    @SerializedName("eventsUrl")
+    private String events_url;
+
+    @SerializedName("received_events_url")
+    private String receivedEventsUrl;
+
+    @SerializedName("type")
+    private String type;
+
+    @SerializedName("site_admin")
+    private boolean siteAdmin;
+
+}
