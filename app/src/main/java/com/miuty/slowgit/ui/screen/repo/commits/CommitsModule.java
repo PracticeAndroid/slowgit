638f73a5c29c8ab614e0a1b016a1d10f8478d1f0
Tran Du <trandu93@outlook.com>
2018-02-04 10:36:00 +0700

    provide layout manager

--- app/src/main/java/com/miuty/slowgit/ui/screen/repo/commits/CommitsModule.java
+++ app/src/main/java/com/miuty/slowgit/ui/screen/repo/commits/CommitsModule.java
@@ -1,8 +1,21 @@
 package com.miuty.slowgit.ui.screen.repo.commits;
 
 
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.miuty.slowgit.di.qualifier.ActivityContext;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class CommitsModule {
+
+    @Provides
+    public RecyclerView.LayoutManager provideLayoutManager(@ActivityContext Context context) {
+        return new LinearLayoutManager(context);
+    }
+
 }
